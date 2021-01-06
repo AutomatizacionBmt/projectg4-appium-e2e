3eaa93792f0205c7bf5a026b26d67d9b5aeb599c
Jose Hurtado <josehumberto17@gmail.com>
2021-01-05 21:58:18 -0500

    first commit

---
+++ src/test/java/com/company/base/BaseTest.java
@@ -0,0 +1,30 @@
+package com.company.base;
+
+import io.appium.java_client.AppiumDriver;
+import org.junit.AfterClass;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest {
+
+    protected static AppiumDriver driver;
+
+    public static void configAppiumDriver(DesiredCapabilities capabilities){
+
+        try {
+            driver = new AppiumDriver(new URL("http://localhost:4723/wd/hub"),
+                    capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.quit();
+    }
+
+
+}
